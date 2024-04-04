7ad9edd549951ecfc4a47b265fba2245dd507638
“Vasia <vpupkin@mail.com>
2024-04-05 01:06:38 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,32 @@ public class Main {
         // Завдання 1
         int array[]= new int[50];
         for (int i = 1, j = 0; i<=99 & j< array.length; i=i+2, j++){
-            array[j]  = i;
+            array[j]  = i;//і значення j індекс
         }
         for(int i = 0; i < array.length; i++)
             System.out.print(array[i]+" ");
 
-
-
-
-
         System.out.println();
         System.out.println("Завдання 2");
+
         //Завдання 2
-        int array0[] = new int[20];
-        for(int i=0; i<20; i++){
-            array0[i] = (int)Math.round(Math.random() * 9);
+        int array0[] = new int[20];//розмір на 20 числ
+        for(int i=0; i<20; i++){//i index
+            array0[i] = (int)(Math.random() * 10);//рандом числа від 0 до 9
         }
         for(int i=0; i<20; i++) {
-            System.out.print(array0[i] + " ");
+            System.out.print(array0[i] + " ");//просто виводимо масим
         }
 
         for(int i=1; i<20; i++) {
-            if (i % 2 != 0) {
-                array0[i] = 0;
+            if (i % 2 != 0) {//перевір на непар
+                array0[i] = 0;//непарн присвой знач 0
             }
         }
         System.out.println();
         for(int i=0; i<20; i++) {
             System.out.print(array0[i] + " ");
         }
-
-
-
-
 
         System.out.println();
         System.out.println("Завдання 3");
@@ -48,27 +41,27 @@ public class Main {
         int array3[] = new int[5];
         double mid1 = 0, mid2 = 0, mid3 = 0;
         for (int i = 0; i < 5; i++) {
-            array1[i] = (int) Math.round(Math.random() * 5);
+            array1[i] = (int)(Math.random() * 5);
             mid1 = mid1 + array1[i];
             System.out.print(array1[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < 5; i++) {
-            array2[i] = (int) Math.round(Math.random() * 5);
+            array2[i] = (int) (Math.random() * 5);
             mid2 = mid2 + array2[i];
             System.out.print(array2[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < 5; i++) {
-            array3[i] = (int) Math.round(Math.random() * 5);
+            array3[i] = (int)(Math.random() * 5);
             mid3 = mid3 + array3[i];
             System.out.print(array3[i] + " ");
         }
         System.out.println();
-        System.out.println(mid1 = mid1 / 5);
+        System.out.println(mid1 = mid1 / 5);//шук сер знач
         System.out.println(mid2 = mid2 / 5);
         System.out.println(mid3 = mid3 / 5);
-        if (mid1 < mid2 & mid3<mid2)
+        if (mid1 < mid2 & mid3<mid2)//шук найб сер знач
             System.out.println(" mid2 ");
         else if (mid1 > mid2 & mid1 > mid3)
             System.out.println(" mid1 ");
@@ -89,17 +82,17 @@ public class Main {
         int array20[] = new int[10];
         int array30[] = new int[10];
         for (int i = 0; i < 10; i++) {
-            array10[i] = (int) Math.round(Math.random() * 10);
+            array10[i] = (int)(Math.random() * 10);
             System.out.print(array10[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < 10; i++) {
-            array20[i] = (int) Math.round(Math.random() * 10);
+            array20[i] = (int)(Math.random() * 10);
             System.out.print(array20[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < 10; i++) {
-            array30[i] = array10[i] + array20[i];
+            array30[i] = array10[i] + array20[i];//ств масив з сум перших елм друг....
             System.out.print(array30[i] + " ");
         }
 
@@ -111,12 +104,12 @@ public class Main {
         System.out.println("Завдання 5");
         //Завдання 5
         int b = 0;
-        int array100[] = new int[15];
+        int array100[] = new int[16];
         for (int i = 0; i < 16; i++) {
-            array100[i] = (int) Math.round(Math.random() * 10);
+            array100[i] = (int)(Math.random() * 10);
             System.out.print(array100[i] + " ");
-            if (array100[i] % 2 == 0)
-                b++;
+            if (array100[i] % 2 == 0)//elem/2 if ==0 +1
+                b++;//kil vsix par elem
         }
         System.out.println();
         System.out.println(b);
@@ -147,7 +140,7 @@ public class Main {
         array12 [14] = new int [9];
         for(int i=0;i<array12.length;i++){//i рядок j стовпець
             for(int j=0;j<array12[i].length;j++){
-                array12[i][j]=(int)(Math.random()*15);
+                array12[i][j]=(int)(Math.random()*15);//в кожен масив дода ранд числа з 0 до 14
                 System.out.print(array12[i][j]+" ");
             }
             System.out.println(" ");
